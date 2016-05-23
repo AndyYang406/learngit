fc2dcb41247d12487d844e3e043515d3a09981fc
AndyYang406 <yanganle2013@163.com>
2016-05-23 15:50:05 +0800

    add a new line and add a word in <readme.txt>, add new file hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Mon 23 May 2016 03:40:11 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "hello, world!" << endl;
+
+	return 0;
+}
